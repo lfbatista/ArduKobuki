f3b990c59a4e5710eb3873066a11ae23a9be9ffe
lfbatista <batistaluisfilipe@gmail.com>
2015-07-30 23:56:41 +0100

    Views and voice feedback

--- Android Client/app/build/generated/source/r/debug/edu/cmu/pocketsphinx/demo/R.java
+++ Android Client/app/build/generated/source/r/debug/edu/cmu/pocketsphinx/demo/R.java
@@ -14,10 +14,25 @@ public final class R {
         public static final int icon=0x7f020000;
     }
     public static final class id {
-        public static final int LinearLayout02=0x7f050002;
-        public static final int button1=0x7f050003;
-        public static final int caption_text=0x7f050000;
-        public static final int result_text=0x7f050001;
+        public static final int LinearLayout01=0x7f050000;
+        public static final int LinearLayout02=0x7f050011;
+        public static final int LinearLayout03=0x7f050002;
+        public static final int LinearLayout04=0x7f050004;
+        public static final int LinearLayout05=0x7f050006;
+        public static final int LinearLayout06=0x7f05000a;
+        public static final int LinearLayout07=0x7f05000c;
+        public static final int buttonActivate=0x7f050001;
+        public static final int buttonDown=0x7f05000b;
+        public static final int buttonExplore=0x7f050003;
+        public static final int buttonFaster=0x7f05000d;
+        public static final int buttonLeft=0x7f050007;
+        public static final int buttonPanic=0x7f050012;
+        public static final int buttonPause=0x7f050008;
+        public static final int buttonRight=0x7f050009;
+        public static final int buttonSlower=0x7f05000e;
+        public static final int buttonUp=0x7f050005;
+        public static final int caption_text=0x7f05000f;
+        public static final int result_text=0x7f050010;
     }
     public static final class layout {
         public static final int main=0x7f030000;
--- Android Client/app/src/main/java/pt/ipg/ardukobuki/controller/MainActivity.java
+++ Android Client/app/src/main/java/pt/ipg/ardukobuki/controller/MainActivity.java
@@ -3,6 +3,8 @@ package pt.ipg.ardukobuki.controller;
 import android.app.Activity;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.speech.tts.TextToSpeech;
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -10,6 +12,7 @@ import android.widget.Toast;
 import java.io.File;
 import java.io.IOException;
 import java.util.HashMap;
+import java.util.Locale;
 
 import edu.cmu.pocketsphinx.Assets;
 import edu.cmu.pocketsphinx.Hypothesis;
@@ -21,7 +24,7 @@ import static android.widget.Toast.makeText;
 import static edu.cmu.pocketsphinx.SpeechRecognizerSetup.defaultSetup;
 
 public class MainActivity extends Activity implements
-        RecognitionListener {
+        RecognitionListener, TextToSpeech.OnInitListener {
 
     private static final String DEVICE_ADDRESS = "20:14:10:15:07:28";
 
@@ -31,10 +34,14 @@ public class MainActivity extends Activity implements
 
     private SpeechRecognizer recognizer;
     private HashMap<String, Integer> captions;
+    private TextToSpeech tts;
 
     @Override
-    public void onCreate(Bundle state) {
-        super.onCreate(state);
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+
+        tts = new TextToSpeech(this, this);
 
         Amarino.connect(this, DEVICE_ADDRESS);
 
@@ -70,10 +77,75 @@ public class MainActivity extends Activity implements
         }.execute();
     }
 
-    public void onClickBtn1(View v)
+    // Views
+    public void onClickBtnActivate(View v)
     {
-        Toast.makeText(this, "Kobuki stoped", Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, "Hello robot!", Toast.LENGTH_SHORT).show();
+        Amarino.sendDataToArduino(this, DEVICE_ADDRESS, ' ', "hello robot");
+        tts.speak("Hello Robot!", TextToSpeech.QUEUE_FLUSH, null);
+    }
+
+    public void onClickBtnExplore(View v)
+    {
+        Toast.makeText(this, "Go explore!", Toast.LENGTH_SHORT).show();
+        Amarino.sendDataToArduino(this, DEVICE_ADDRESS, ' ', "go explore");
+        tts.speak("Kobuki, explore!", TextToSpeech.QUEUE_FLUSH, null);
+    }
+
+    public void onClickBtnUp(View v)
+    {
+        Toast.makeText(this, "Go forward!", Toast.LENGTH_SHORT).show();
+        Amarino.sendDataToArduino(this, DEVICE_ADDRESS, ' ', "go forward");
+        tts.speak("Kobuki, go forward!", TextToSpeech.QUEUE_FLUSH, null);
+    }
+
+    public void onClickBtnLeft(View v)
+    {
+        Toast.makeText(this, "Turn left!", Toast.LENGTH_SHORT).show();
+        Amarino.sendDataToArduino(this, DEVICE_ADDRESS, ' ', "turn left");
+        tts.speak("Kobuki, turn left!", TextToSpeech.QUEUE_FLUSH, null);
+    }
+
+    public void onClickBtnPause(View v)
+    {
+        Toast.makeText(this, "Stop!", Toast.LENGTH_SHORT).show();
         Amarino.sendDataToArduino(this, DEVICE_ADDRESS, ' ', "stop");
+        tts.speak("Kobuki, stop!", TextToSpeech.QUEUE_FLUSH, null);
+    }
+
+    public void onClickBtnRight(View v)
+    {
+        Toast.makeText(this, "Turn right!", Toast.LENGTH_SHORT).show();
+        Amarino.sendDataToArduino(this, DEVICE_ADDRESS, ' ', "turn right");
+        tts.speak("Kobuki, turn right!", TextToSpeech.QUEUE_FLUSH, null);
+    }
+
+    public void onClickBtnDown(View v)
+    {
+        Toast.makeText(this, "Go backwards!", Toast.LENGTH_SHORT).show();
+        Amarino.sendDataToArduino(this, DEVICE_ADDRESS, ' ', "backwards");
+        tts.speak("Kobuki, go backwards!", TextToSpeech.QUEUE_FLUSH, null);
+    }
+
+    public void onClickBtnFaster(View v)
+    {
+        Toast.makeText(this, "Go faster!", Toast.LENGTH_SHORT).show();
+        Amarino.sendDataToArduino(this, DEVICE_ADDRESS, ' ', "go faster");
+        tts.speak("Kobuki, go faster!", TextToSpeech.QUEUE_FLUSH, null);
+    }
+
+    public void onClickBtnSlower(View v)
+    {
+        Toast.makeText(this, "Go slower!", Toast.LENGTH_SHORT).show();
+        Amarino.sendDataToArduino(this, DEVICE_ADDRESS, ' ', "go slower");
+        tts.speak("Kobuki, go slower!", TextToSpeech.QUEUE_FLUSH, null);
+    }
+
+    public void onClickBtnPanic(View v)
+    {
+        Toast.makeText(this, "Kobuki stoped.", Toast.LENGTH_SHORT).show();
+        Amarino.sendDataToArduino(this, DEVICE_ADDRESS, ' ', "exit");
+        tts.speak("Kobuki stoped.", TextToSpeech.QUEUE_FLUSH, null);
     }
 
     @Override
@@ -84,14 +156,73 @@ public class MainActivity extends Activity implements
 
     @Override
     public void onDestroy() {
+        if (tts != null) {
+            tts.stop();
+            tts.shutdown();
+        }
         super.onDestroy();
-        //  recognizer.cancel();
-     //   recognizer.shutdown();
     }
 
     @Override
-    public void onStop(){
-        super.onStop();
+    public void onInit(int status) {
+        // TODO Auto-generated method stub
+
+        if (status == TextToSpeech.SUCCESS) {
+            int result = tts.setLanguage(Locale.US);
+
+            if (result == TextToSpeech.LANG_MISSING_DATA
+                    || result == TextToSpeech.LANG_NOT_SUPPORTED) {
+                Log.e("TTS", "Language is not supported");
+            } else {
+                Log.e("TTS", "Everything is set...");
+            }
+
+        } else {
+            Log.e("TTS", "Initilization Failed");
+        }
+    }
+
+    private void switchSearch(String searchName) {
+        recognizer.stop();
+
+        if (searchName.equals(KWS_SEARCH))
+            recognizer.startListening(searchName);
+        else
+            recognizer.startListening(searchName, 20000);
+
+        String caption = getResources().getString(captions.get(searchName));
+        ((TextView) findViewById(R.id.caption_text)).setText(caption);
+    }
+
+    @Override
+    public void onBeginningOfSpeech() {    }
+
+    private void setupRecognizer(File assetsDir) throws IOException {
+        // The recognizer can be configured to perform multiple searches
+        // of different kind and switch between them
+        recognizer = defaultSetup()
+                .setAcousticModel(new File(assetsDir, "en-us-ptm"))
+                .setDictionary(new File(assetsDir, "cmudict-en-us.dict"))
+                        // To disable logging of raw audio comment out this call (takes a lot of space on the device)
+                .setRawLogDir(assetsDir)
+                        // Threshold to tune for keyphrase to balance between false alarms and misses
+                .setKeywordThreshold(1e-45f)
+                        // Use context-independent phonetic search, context-dependent is too slow for mobile
+                .setBoolean("-allphone_ci", true)
+                .getRecognizer();
+        recognizer.addListener(this);
+        recognizer.addKeyphraseSearch(KWS_SEARCH, KEYPHRASE);
+
+        // Grammar
+        File commandsGrammar = new File(assetsDir, "commands.gram");
+        recognizer.addGrammarSearch(COMMANDS_SEARCH, commandsGrammar);
+        recognizer.startListening(COMMANDS_SEARCH, 10000);
+    }
+
+    @Override
+    public void onEndOfSpeech() {
+        if (!recognizer.getSearchName().equals(KWS_SEARCH))
+            switchSearch(KWS_SEARCH);
     }
 
     @Override
@@ -109,59 +240,17 @@ public class MainActivity extends Activity implements
         ((TextView) findViewById(R.id.result_text)).setText("");
         if (hypothesis != null) {
             String text = hypothesis.getHypstr();
-            makeText(getApplicationContext(), text, Toast.LENGTH_SHORT).show();
+            makeText(getApplicationContext(), text + "!", Toast.LENGTH_SHORT).show();
+            tts.speak("Kobuki!" + text, TextToSpeech.QUEUE_FLUSH, null);
             Amarino.sendDataToArduino(this, DEVICE_ADDRESS, ' ', text);
         }
-    }
-
-    @Override
-    public void onBeginningOfSpeech() {}
-
-    @Override
-    public void onEndOfSpeech() {
-        if (!recognizer.getSearchName().equals(KWS_SEARCH))
-            switchSearch(KWS_SEARCH);
-    }
-
-    private void switchSearch(String searchName) {
-        recognizer.stop();
-
-        if (searchName.equals(KWS_SEARCH))
-            recognizer.startListening(searchName);
-        else
-            recognizer.startListening(searchName, 20000);
-
-        String caption = getResources().getString(captions.get(searchName));
-        ((TextView) findViewById(R.id.caption_text)).setText(caption);
-    }
-
-    private void setupRecognizer(File assetsDir) throws IOException {
-        recognizer = defaultSetup()
-                .setAcousticModel(new File(assetsDir, "en-us-ptm"))
-                .setDictionary(new File(assetsDir, "cmudict-en-us.dict"))
-                // To disable logging of raw audio comment out this call (takes a lot of space on the device)
-                .setRawLogDir(assetsDir)
-                // Threshold to tune for keyphrase to balance between false alarms and misses
-                .setKeywordThreshold(1e-45f)
-                // Use context-independent phonetic search, context-dependent is too slow for mobile
-                .setBoolean("-allphone_ci", true)
-                .getRecognizer();
-        recognizer.addListener(this);
-
-        recognizer.addKeyphraseSearch(KWS_SEARCH, KEYPHRASE);
-
-        File commandsGrammar = new File(assetsDir, "commands.gram");
-        recognizer.addGrammarSearch(COMMANDS_SEARCH, commandsGrammar);
-        recognizer.startListening(COMMANDS_SEARCH, 10000);
-    }
-
-    @Override
-    public void onError(Exception error) {
-        ((TextView) findViewById(R.id.caption_text)).setText(error.getMessage());
     }
 
     @Override
     public void onTimeout() {
         switchSearch(KWS_SEARCH);
     }
+
+    @Override
+    public void onError(Exception e) {    }
 }
